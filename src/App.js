af7487329bac5964896dbcd11f315dd97e4d4bfb
anafmt <ana.fmtavares@gmail.com>
2023-10-09 15:54:27 +0200

    Added rel="noreferrer" to links

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default function App() {
     <div className="App">
        <h1>Weather App</h1> 
       <footer>
-        This project was coded by <a href='https://www.linkedin.com/in/ana-fm-tavares/' target='_blank'>Ana FM Tavares </a> and is 
-      <a href='https://github.com/anafmt/react-weather-app' target='_blank'> open-sourced on GitHub</a>
+        This project was coded by <a href='https://www.linkedin.com/in/ana-fm-tavares/' target='_blank' rel="noreferrer">Ana FM Tavares </a> and is 
+      <a href='https://github.com/anafmt/react-weather-app' target='_blank' rel="noreferrer"> open-sourced on GitHub</a>
       </footer>
     </div>
   );
