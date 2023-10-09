5f9d01b430e7cb5d0d00c3dfa6571c6a3a9fb3d0
anafmt <ana.fmtavares@gmail.com>
2023-10-09 15:21:22 +0200

    Added the GitHup repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-        Weather App!
+       <h1>Weather App</h1> 
+      <footer>
+        This project was coded by <a href='https://www.linkedin.com/in/ana-fm-tavares/' target='_blank'>Ana FM Tavares </a> and is 
+      <a href='https://github.com/anafmt/react-weather-app' target='_blank'> open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
