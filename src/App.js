a9e2c9ec749709909b85c763516629e09fe465cc
anafmt <ana.fmtavares@gmail.com>
2023-10-10 16:58:04 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Weather from './Weather';
 
 
 export default function App() {
   return (
     <div className="App">
-      <div className='container'>
-        <h1>Weather App</h1> 
+      <div className="container">
+        <Weather />
         <footer>
           This project was coded by <a href='https://www.linkedin.com/in/ana-fm-tavares/' target='_blank' rel="noreferrer">Ana FM Tavares </a> and is 
           <a href='https://github.com/anafmt/react-weather-app' target='_blank' rel="noreferrer"> open-sourced on GitHub</a>
