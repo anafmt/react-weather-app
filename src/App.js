d278e5e3d2adcba062de1efe77fe4c570475b144
anafmt <ana.fmtavares@gmail.com>
2023-10-10 14:41:19 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
 
+
 export default function App() {
   return (
     <div className="App">
-       <h1>Weather App</h1> 
-      <footer>
-        This project was coded by <a href='https://www.linkedin.com/in/ana-fm-tavares/' target='_blank' rel="noreferrer">Ana FM Tavares </a> and is 
-      <a href='https://github.com/anafmt/react-weather-app' target='_blank' rel="noreferrer"> open-sourced on GitHub</a>
-      </footer>
+      <div className='container'>
+        <h1>Weather App</h1> 
+        <footer>
+          This project was coded by <a href='https://www.linkedin.com/in/ana-fm-tavares/' target='_blank' rel="noreferrer">Ana FM Tavares </a> and is 
+          <a href='https://github.com/anafmt/react-weather-app' target='_blank' rel="noreferrer"> open-sourced on GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
